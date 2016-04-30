b6db2a856dfd348d207a565a21eb2c5924088582
lubien <lubien1996@gmail.com>
2016-04-30 18:06:13 -0300

    Initial

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+const koa = require('koa');
+const koaRouter = require('koa-router');
+
+const app = koa();
+const router = koaRouter();
+
+router.get('/', function *() {
+  this.body = 'Hello World';
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+let port = process.env.PORT || 3000;
+let ip = process.env.IP || '0.0.0.0';
+app.listen(port, ip, () => {
+  console.log(`Server listening at ${ip}:${port}`);
+});
