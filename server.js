b40e4cee7aa852b92178885873cf05ab4c315a10
lubien <lubien1996@gmail.com>
2016-05-04 23:44:47 -0300

    Fix typo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const BotFactory = require('messenger-bot');
 
 const bot = new BotFactory({
   token: 'EAAIvoT1Uwy4BANjTEMmbD7NZCCkTD9CZCZAmJF8MFq7EagIU2ObOZBhFFfOx3pLgb8jvuS3oZB14wQWi320ZCrb2hY6JbVK3GL9v4C617IRw8jcIs51yZCN5vZBDbvYbuS4s7ausLx9rovFQV0gXuDFQ9nrvh3MBorZC2BxflRdGjJAZDZD',
-  veify: 'you_shall_not_pass',
+  verify: 'you_shall_not_pass',
 });
 
 bot.on('error', err => {
